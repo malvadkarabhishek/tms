3d5fac62328d6141bb5edcd7c15d6f0ef0d6636c
Abhishek Malvadkar <malvadab46@gmail.com>
2020-04-29 19:30:05 +0530

    Delete all integration and logo changed.

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 
+
+
+
+
 // database
-var db  = null;
+var db = null;
 
 var taskList = [];
+
+
 
 var noDataText = 'No data to display';
 
@@ -15,14 +21,21 @@ const request = indexedDB.open('tms');
 // upgrade
 request.onupgradeneeded = e => {
     db = e.target.result;
-    const result = db.createObjectStore('tasks',{keyPath:'id',autoIncrement:true});
+    const result = db.createObjectStore('tasks', { keyPath: 'id', autoIncrement: true });
 
 }
 
 // success
 request.onsuccess = e => {
     db = e.target.result;
-    getTasks();
+        getTasks();
+    // if (location.href.includes("edit")) {
+    //     var currentUrl = window.location.href;
+    //     var urlArray = currentUrl.split("=");
+    //     editId = parseInt(urlArray[1]);
+    //     getTask();
+    // }
+
 
 }
 
@@ -35,25 +48,38 @@ request.onerror = e => {
 const addTask = () => {
     let taskName = document.getElementById('tname');
     let task = {
-        name:taskName.value,
-        status:'Pending'
+        name: taskName.value,
+        status: 'Pending'
     };
-    const tx = db.transaction('tasks','readwrite');
+    const tx = db.transaction('tasks', 'readwrite');
     var os = tx.objectStore('tasks');
     const request = os.add(task);
+
     request.onsuccess = (res) => {
-        window.location.href ='./index.html';
+        window.location.href = './index.html';
     }
     request.onerror = (e) => {
-        
+
     }
 };
+
+function success() {
+    if (document.getElementById("tname").value.length > 0) {
+        document.getElementById('addBtn').disabled = false;
+    } else {
+        document.getElementById('addBtn').disabled = true;
+    }
+}
+
+function disableAdd() {
+    document.getElementById('addBtn').disabled = true;
+}
 
 // get all tasks
 
 const getTasks = () => {
     taskList = [];
-    const tx = db.transaction('tasks','readwrite');
+    const tx = db.transaction('tasks', 'readwrite');
     var os = tx.objectStore('tasks');
 
     var request = os.openCursor();
@@ -75,21 +101,25 @@ const createTableContent = (taskList) => {
     var html = "";
     if (taskList && taskList.length) {
         taskList.forEach(task => {
-            html+="<tr>";
-            html+="<td class='text-center'>"+task.name+"</td>";
-            html+="<td class='text-center'><span class='status'>"+task.status+"</span></td>";
-            html+="<td class='text-center'><a href='#' class='disable'><i class='fa fa-pencil' aria-hidden='true'></i></a>&nbsp;<a href='#' class='disable' aria-disabled='true'><i class='fa fa-trash'></i></a></td>";
-    
-            html+="</tr>";
-        })
-       
-    } else {
-        html+="<tr>";
-        html+="<td colspan='3' class='text-center''>"+noDataText+"</td>";
+            html += "<tr>";
+            html += "<td class='data'>" + task.name + "</td>";
+            // html += "<td class='text-center'><span class='status'>" + task.status + "</span></td>";
+            html += "<td class='text-center'><a class='btn btn-danger disable'>Delete</a></td>";
 
-        html+="</tr>";
+            html += "</tr>";
+        })
+
+    } else {
+        html += "<tr>";
+        html += "<td colspan='3' class='text-center''>" + noDataText + "</td>";
+
+        html += "</tr>";
     }
     $(".table-content").html(html);
 }
 
 
+const deleteDatabase = () => {
+    var request = indexedDB.deleteDatabase('tms');
+    window.location.href = './index.html';
+}
